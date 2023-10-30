202493fce7c166a33fd69484e6992ae353a3de8d
chris <chrismulcair@gmail.com>
2023-10-30 17:38:11 +1100

    add set_name to bill

--- hello.go
+++ hello.go
@@ -7,12 +7,23 @@ type bill struct {
 	amount int
 }
 
+func (b *bill) set_name(the_name string) {
+	b.name = the_name
+}
+
+func set_name_outside(b *bill, the_name string) {
+	b.name = the_name
+}
+
 func main() {
 
 	bill1 := bill{
 		name:   "chris",
 		amount: 99,
 	}
+
+	bill1.set_name("test")
+	set_name_outside(&bill1, "shit")
 	fmt.Println(bill1.name)
 
 }
