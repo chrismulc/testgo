f1e272b17520446114d251f101bad2ebc4a64409
chris <chrismulcair@gmail.com>
2023-11-07 16:44:20 +1100

    comment also in test

--- go_bill/hello.go
+++ go_bill/hello.go
@@ -87,6 +87,7 @@ func main() {
 	var my_test mypackage.Faker
 	my_test.Name = "Chris"
 	// only in test
+	// also only in test
 
 	fmt.Printf("%s", my_test.Name)
 	billtest, _ := readJsonFile("bill.json")
