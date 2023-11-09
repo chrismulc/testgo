66d349375cb25257b2d364d2bd8c8d98f3b8dc79
Chris Mulcair <chrismulcair@gmail.com>
2023-11-09 12:14:00 +1100

    add second Bill example

--- go_bill/hello.go
+++ go_bill/hello.go
@@ -11,11 +11,13 @@ func main() {
 	// this is in test
 	var my_test mypackage.Faker
 	my_test.Name = "Chris"
+  var crap *mypackage.Bill
+  crap, _ = mypackage.ReadJsonFile("bill.json")
 	// only in test
 	// also only in test
 
 	fmt.Printf("%s", my_test.Name)
   billtest, _ := mypackage.ReadJsonFile("bill.json")
 	fmt.Printf("Bill : %+v\n", billtest)
-
+  fmt.Printf("Crap : %+v\n", crap)
 }
