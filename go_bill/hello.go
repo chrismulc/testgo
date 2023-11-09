f7ec436a81837c8a88af8556e5192df5c1780b48
Chris Mulcair <chrismulcair@gmail.com>
2023-11-09 12:37:40 +1100

    add comment to test vscode

--- go_bill/hello.go
+++ go_bill/hello.go
@@ -1,23 +1,23 @@
- package main
+package main
 
 import (
 	"fmt"
 	"go_bill/mypackage"
 )
 
-
 func main() {
 
 	// this is in test
 	var my_test mypackage.Faker
 	my_test.Name = "Chris"
-  var crap *mypackage.Bill
-  crap, _ = mypackage.ReadJsonFile("bill.json")
+	var crap *mypackage.Bill
+	crap, _ = mypackage.ReadJsonFile("bill.json")
 	// only in test
 	// also only in test
 
 	fmt.Printf("%s", my_test.Name)
-  billtest, _ := mypackage.ReadJsonFile("bill.json")
+	billtest, _ := mypackage.ReadJsonFile("bill.json")
 	fmt.Printf("Bill : %+v\n", billtest)
-  fmt.Printf("Crap : %+v\n", crap)
+	fmt.Printf("Crap : %+v\n", crap)
+	// vscode
 }
